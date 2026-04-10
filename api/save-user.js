2285399baf5343146694a2dc9125b8842286c6d4
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-04-10 10:28:23 -0600

    Update save-user.js

--- api/save-user.js
+++ api/save-user.js
@@ -7,3 +7,15 @@ export default function handler(req, res) {
 
   res.json({ success: true });
 }
+
+import { supabase } from "../lib/supabase";
+
+export default async function handler(req, res) {
+  const { email, site, result } = req.body;
+
+  await supabase.from("reports").insert([
+    { email, site, result }
+  ]);
+
+  res.json({ success: true });
+}
